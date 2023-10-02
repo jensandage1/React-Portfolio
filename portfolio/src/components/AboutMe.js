10114b92df84240ece7ed33dad87089e3054c9f5
Jen Sandage <jensandage1@gmail.com>
2023-10-02 16:37:32 -0700

    changed color pallet so there wasn't a bright green

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -7,10 +7,8 @@ export default function AboutMe(){
     return (
         <div className="about">
             <h1 id="about-me-title"> About Me </h1>
-            <img src={profilePicture} alt="Jen Sandage"/>
-            <p>I am an aspiring developer with a background in education and psychology. I spent the last several years working in schools. I developed art projects from the ground up, and managed their production through my students. I am looking for new career that will allow me to use my creativity and management skills as well as my new developer skills.</p>
+            <img id="profile-pic" src={profilePicture} alt="Jen Sandage"/>
+            <p id="bio">I am an aspiring developer with a background in education and psychology. I spent the last several years working in schools. I developed art projects from the ground up, and managed their production through my students. I am looking for new career that will allow me to use my creativity and management skills as well as my new developer skills.</p>
         </div>
     );
 }
-
-//css for formatting
--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -14,37 +14,37 @@ export default function Portfolio(){
            <div className="flex-container">
             <figure>
                 <h3 className="app-title">Pet Adoption</h3>
-                <img src={socialWhiskers} alt="screenshot of Social Whiskers App"/>
+                <img className="portfolio-img"src={socialWhiskers} alt="screenshot of Social Whiskers App"/>
                 <figcaption><a className="links"href="https://social-whiskers-ad907fdaaf47.herokuapp.com/">View App</a></figcaption>
                 <figcaption><a className="links"href="https://github.com/edwinalban/Social-Whiskers-Pet-Adoption">View Repository</a></figcaption>
             </figure>
             <figure>
                 <h3 className="app-title">News Compiler</h3>
-                <img src={newsCompiler} alt="screenshot of News Compiler App"/>
+                <img className="portfolio-img"src={newsCompiler} alt="screenshot of News Compiler App"/>
                 <figcaption><a className="links"href="https://jensandage1.github.io/News-API-app-with-Definition-Searching/">View App</a></figcaption>
                 <figcaption><a className="links"href="https://github.com/jensandage1/News-API-app-with-Definition-Searching">View Repository</a></figcaption>
             </figure>
             <figure>
                 <h3 className="app-title">Text Editor</h3>
-                <img src={textEditor} alt="screenshot of text editor App"/>
+                <img className="portfolio-img"src={textEditor} alt="screenshot of text editor App"/>
                 <figcaption><a className="links"href="https://jate-text-editor-mod-19-41afa2aa7929.herokuapp.com/">View App</a></figcaption>
                 <figcaption><a className="links"href="https://github.com/jensandage1/Progressive-Web-App-Text-Editor">View Repository</a></figcaption>
             </figure>
             <figure>
                 <h3 className="app-title">Note Taker</h3>
-                <img src={noteTaker} alt="screenshot of note taker App"/>
+                <img className="portfolio-img"src={noteTaker} alt="screenshot of note taker App"/>
                 <figcaption><a className="links"href="https://module-11-note-taker-challenge-7c52ae029d38.herokuapp.com/">View App</a></figcaption>
                 <figcaption><a className="links"href="https://github.com/jensandage1/Module-11-Challenge-Note-Taker">View Repository</a></figcaption>
             </figure>
             <figure>
                 <h3 className="app-title">Weather Dashboard</h3>
-                <img src={weatherApp} alt="screenshot of weather app"/>
+                <img className="portfolio-img"src={weatherApp} alt="screenshot of weather app"/>
                 <figcaption><a className="links"href="https://jensandage1.github.io/module-6-challenge-weather-dashboard/">View App</a></figcaption>
                 <figcaption><a className="links"href="https://github.com/jensandage1/module-6-challenge-weather-dashboard">View Repository</a></figcaption>
             </figure>
             <figure>
                 <h3 className="app-title">Portfolio</h3>
-                <img src ={portfolio} alt="screenshot of non-React portfolio" />
+                <img className="portfolio-img"src ={portfolio} alt="screenshot of non-React portfolio" />
                 <figcaption><a className="links"href="https://jensandage1.github.io/module-2-challenge-portfolio/">View App</a></figcaption>
                 <figcaption><a className="links"href="https://github.com/jensandage1/module-2-challenge-portfolio">View Repository</a></figcaption>
             </figure>
